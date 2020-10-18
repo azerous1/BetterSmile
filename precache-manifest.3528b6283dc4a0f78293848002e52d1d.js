d2d3776f531aa745e6ce3d5242eabb315f344a5a
Yichi Zhang <arthur233333@gmail.com>
2020-10-18 04:47:43 -0700

    Updates

---
+++ precache-manifest.3528b6283dc4a0f78293848002e52d1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d64cc50d2a97274075fdd86f1e4690",
+    "url": "/BetterSmile/index.html"
+  },
+  {
+    "revision": "91f18b7dededd06c0166",
+    "url": "/BetterSmile/static/css/main.7ec6035e.chunk.css"
+  },
+  {
+    "revision": "bf2677345dc54591d5a7",
+    "url": "/BetterSmile/static/js/2.29f9ec65.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/BetterSmile/static/js/2.29f9ec65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f18b7dededd06c0166",
+    "url": "/BetterSmile/static/js/main.f1978f74.chunk.js"
+  },
+  {
+    "revision": "ace819e6f1af0d95a4aa",
+    "url": "/BetterSmile/static/js/runtime-main.60eb748a.js"
+  },
+  {
+    "revision": "cfb69f1820c226e7e803356727a813ba",
+    "url": "/BetterSmile/static/media/logo.cfb69f18.png"
+  },
+  {
+    "revision": "04a1f0967f6870fb0e1f662a004e52ed",
+    "url": "/BetterSmile/static/media/mouse.04a1f096.jpg"
+  },
+  {
+    "revision": "9c85adadedf74fcdbd5923aee55a7ec7",
+    "url": "/BetterSmile/static/media/smile.9c85adad.png"
+  },
+  {
+    "revision": "8a21d786c425ab02c2f67ac6379d38a5",
+    "url": "/BetterSmile/static/media/teeth.8a21d786.png"
+  },
+  {
+    "revision": "ddafad0469910871c2730e7bb5eedf03",
+    "url": "/BetterSmile/static/media/tongue.ddafad04.png"
+  }
+]);
